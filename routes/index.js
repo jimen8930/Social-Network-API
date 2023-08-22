2a0081d544d6adaa498485625b2b3d4ede10aebf
jimen8930 <jimen8930@aol.com>
2023-08-22 18:12:54 -0400

    Added comments to the index.js file in the api folder.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
+// Imports for express and api pathway
 const router = require('express').Router();
 const apiRoutes = require('./api');
-
+// middleware for the routes
 router.use('/api', apiRoutes);
 
 router.use((req, res) => res.send('Wrong route!'));
-
+// export
 module.exports = router;
